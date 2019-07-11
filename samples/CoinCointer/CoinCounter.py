50ab9200f31b92a42c817b7a72024e89155e7493
jamesgleave <j.gleave.james@gmail.com>
2019-07-11 10:32:48 -0700

    Added an inference command

--- samples/CoinCointer/CoinCounter.py
+++ samples/CoinCointer/CoinCounter.py
@@ -373,81 +373,31 @@ sys.path.append(os.path.join(ROOT_DIR, "samples/CoinCoiter/"))  # To find local 
 MODEL_DIR = os.path.join(ROOT_DIR, "logs")
 
 # Local path to trained weights file
-CoinCounter_MODEL_PATH = os.path.join(ROOT_DIR, "logs/mask_rcnn_coco.h5")
-# Download COCO trained weights from Releases if needed
-if not os.path.exists(CoinCounter_MODEL_PATH):
-    utils.download_trained_weights(CoinCounter_MODEL_PATH)
+CoinCounter_MODEL_PATH = os.path.join(ROOT_DIR, "logs/CoinCounterLogsTwo__LowerLR_and_LowerMMNTM/coin20190710T2036"
+                                                "mask_rcnn_coin_0030.h5")
 
 # Directory of images to run detection on
-IMAGE_DIR = os.path.join(ROOT_DIR, "images")
+IMAGE_DIR = os.path.join(ROOT_DIR, "datasets/coin/val")
 
-
-class InferenceConfig(CoinConfig):
-    # Set batch size to 1 since we'll be running inference on
-    # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
-    GPU_COUNT = 1
-    IMAGES_PER_GPU = 1
-
-
-config = InferenceConfig()
-config.display()
-
-# Create model object in inference mode.
-model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
-
-# Load weights trained on MS-COCO
-model.load_weights(CoinCounter_MODEL_PATH, by_name=True)
-
-# COCO Class names
-# Index of the class in the list is its ID. For example, to get ID of
-# the teddy bear class, use: class_names.index('teddy bear')
-class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
-               'bus', 'train', 'truck', 'boat', 'traffic light',
-               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
-               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
-               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
-               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
-               'kite', 'baseball bat', 'baseball glove', 'skateboard',
-               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
-               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
-               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
-               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
-               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
-               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
-               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
-               'teddy bear', 'hair drier', 'toothbrush']
+# My Class names
+ds = CoinDataset()
+class_names = ds.class_names()
 
 # Load a random image from the images folder
 # file_names = next(os.walk(IMAGE_DIR))[2]
 # image = skimage.io.imread(os.path.join(IMAGE_DIR, random.choice(file_names)))
 
 
-def runThroughDir(filepath):
-    image_list = []
-    for filename in glob.glob(filepath + '/*.jpg'):
-        im = skimage.io.imread(filename)
-        image_list.append(im)
-
-
-    # Run detection
-    for image in image_list:
-        results = model.detect([image], verbose=1)
-
-        # Visualize results
-        r = results[0]
-        visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'],
-                                    class_names, r['scores'])
-
-
-def defineImage(filePath):
-    im = skimage.io.imread(filePath)
-    results = model.detect([im], verbose=1)
+def define_image(file_path, model_inf):
+    im = skimage.io.imread(file_path)
+    results = model_inf.detect([im], verbose=1)
     r = results[0]
     visualize.display_instances(im, r['rois'], r['masks'], r['class_ids'],
-                                    class_names, r['scores'])
+                                class_names, r['scores'])
 
-# Enter the path of an image here.
-defineImage("Path/To/Image/")
+
+def inference(path, model_inf):
+    define_image(path, model_inf)
 
 
 ############################################################
@@ -463,7 +413,7 @@ if __name__ == '__main__':
         description='Train Mask R-CNN to detect coins.')
     parser.add_argument("command",
                         metavar="<command>",
-                        help="'train' or 'splash'")
+                        help="'train', 'splash' or 'inference'")
     parser.add_argument('--dataset', required=False,
                         metavar="/path/to/coin/dataset/",
                         help='Directory of the coin dataset')
@@ -480,6 +430,7 @@ if __name__ == '__main__':
     parser.add_argument('--video', required=False,
                         metavar="path or URL to video",
                         help='Video to apply the color splash effect on')
+
     args = parser.parse_args()
 
     # Validate arguments
@@ -488,6 +439,9 @@ if __name__ == '__main__':
     elif args.command == "splash":
         assert args.image or args.video,\
                "Provide --image or --video to apply color splash"
+    elif args.command == "inference":
+        assert args.image,\
+               "Provide --image= (image path) or (directory path)"
 
     print("Weights: ", args.weights)
     print("Dataset: ", args.dataset)
@@ -512,6 +466,9 @@ if __name__ == '__main__':
     else:
         model = modellib.MaskRCNN(mode="inference", config=config,
                                   model_dir=args.logs)
+
+    if args.command == "inference":
+        inference(model=model, path=args.image)
 
     # Select weights file to load
     if args.weights.lower() == "coco":
