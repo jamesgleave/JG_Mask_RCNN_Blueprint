cef1f5bab118a26f97f6f4618792296f14b74cdf
jamesgleave <j.gleave.james@gmail.com>
2019-07-11 10:08:57 -0700

    Added large components

--- samples/CoinCointer/CoinCounter.py
+++ samples/CoinCointer/CoinCounter.py
@@ -7,6 +7,8 @@ import skimage.draw
 import skimage.color
 import skimage.io
 import skimage.transform as skit
+import glob
+
 
 # Root directory of the project
 ROOT_DIR = os.path.abspath("../../")
@@ -15,6 +17,7 @@ ROOT_DIR = os.path.abspath("../../")
 sys.path.append(ROOT_DIR)  # To find local version of the library
 from mrcnn.config import Config
 from mrcnn import model as modellib, utils
+from mrcnn import visualize
 
 # Path to trained weights file
 COCO_WEIGHTS_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
@@ -356,6 +359,95 @@ def detect_and_color_splash(model, image_path=None, video_path=None):
                 count += 1
         vwriter.release()
     print("Saved to ", file_name)
+
+
+############################################################
+#  Inference
+############################################################
+
+
+# Import my config
+sys.path.append(os.path.join(ROOT_DIR, "samples/CoinCoiter/"))  # To find local version
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Local path to trained weights file
+CoinCounter_MODEL_PATH = os.path.join(ROOT_DIR, "logs/mask_rcnn_coco.h5")
+# Download COCO trained weights from Releases if needed
+if not os.path.exists(CoinCounter_MODEL_PATH):
+    utils.download_trained_weights(CoinCounter_MODEL_PATH)
+
+# Directory of images to run detection on
+IMAGE_DIR = os.path.join(ROOT_DIR, "images")
+
+
+class InferenceConfig(CoinConfig):
+    # Set batch size to 1 since we'll be running inference on
+    # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+
+config = InferenceConfig()
+config.display()
+
+# Create model object in inference mode.
+model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+
+# Load weights trained on MS-COCO
+model.load_weights(CoinCounter_MODEL_PATH, by_name=True)
+
+# COCO Class names
+# Index of the class in the list is its ID. For example, to get ID of
+# the teddy bear class, use: class_names.index('teddy bear')
+class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+               'bus', 'train', 'truck', 'boat', 'traffic light',
+               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+               'kite', 'baseball bat', 'baseball glove', 'skateboard',
+               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+               'teddy bear', 'hair drier', 'toothbrush']
+
+# Load a random image from the images folder
+# file_names = next(os.walk(IMAGE_DIR))[2]
+# image = skimage.io.imread(os.path.join(IMAGE_DIR, random.choice(file_names)))
+
+
+def runThroughDir(filepath):
+    image_list = []
+    for filename in glob.glob(filepath + '/*.jpg'):
+        im = skimage.io.imread(filename)
+        image_list.append(im)
+
+
+    # Run detection
+    for image in image_list:
+        results = model.detect([image], verbose=1)
+
+        # Visualize results
+        r = results[0]
+        visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'],
+                                    class_names, r['scores'])
+
+
+def defineImage(filePath):
+    im = skimage.io.imread(filePath)
+    results = model.detect([im], verbose=1)
+    r = results[0]
+    visualize.display_instances(im, r['rois'], r['masks'], r['class_ids'],
+                                    class_names, r['scores'])
+
+# Enter the path of an image here.
+defineImage("Path/To/Image/")
 
 
 ############################################################
