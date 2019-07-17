055c2575b0ee0a136852a8897306e8e865ed5e3b
james <j.gleave.james@gmail.com>
2019-07-16 19:18:05 -0700

    updated skeleton blueprint

--- samples/CoinCointer/CoinCounter.py
+++ samples/CoinCointer/CoinCounter.py
@@ -403,7 +403,8 @@ def optimize_hyperparameters(log_path, benchmark_model, num_of_cylces=30, epochs
             print("value of loss:", val, "at index", i)
 
         loss = model_hpo.keras_model.total_loss
-        np.array(loss)
+        loss = np.array(loss)
+        print(loss)
         print("loss:", loss)
 
         loss_config_name = (loss, model_hpo.config, model_hpo.config.NAME)
