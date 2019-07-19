3d0b97248fc44d8f4d43839b11d7fdf336496505
jamesgleave <j.gleave.james@gmail.com>
2019-07-19 12:57:39 -0700

    testing

--- samples/CoinCointer/CoinCounter.py
+++ samples/CoinCointer/CoinCounter.py
@@ -3,6 +3,8 @@ os.environ['MKL_NUM_THREADS'] = '16'
 os.environ['GOTO_NUM_THREADS'] = '16'
 os.environ['OMP_NUM_THREADS'] = '16'
 os.environ['openmp'] = 'True'
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
 import sys
 import json
 import datetime
