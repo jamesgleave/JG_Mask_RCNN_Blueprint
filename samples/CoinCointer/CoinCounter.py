1a33cc52ee70111e34f5a024a9331a1b87e23943
jamesgleave <j.gleave.james@gmail.com>
2019-07-19 12:04:01 -0700

    testing

--- samples/CoinCointer/CoinCounter.py
+++ samples/CoinCointer/CoinCounter.py
@@ -764,8 +764,8 @@ if __name__ == '__main__':
     from keras import backend as K
     import tensorflow as tf
 
-    NUM_PARALLEL_EXEC_UNITS = 32
-    config = tf.ConfigProto(intra_op_parallelism_threads=NUM_PARALLEL_EXEC_UNITS, inter_op_parallelism_threads=2,
+    NUM_PARALLEL_EXEC_UNITS = 8
+    config = tf.ConfigProto(intra_op_parallelism_threads=NUM_PARALLEL_EXEC_UNITS, inter_op_parallelism_threads=4,
                             allow_soft_placement=True, device_count={'CPU': NUM_PARALLEL_EXEC_UNITS})
 
     config.graph_options.optimizer_options.global_jit_level = tf.OptimizerOptions.ON_1
@@ -773,10 +773,10 @@ if __name__ == '__main__':
     session = tf.Session(config=config)
     K.set_session(session)
 
-    os.environ["OMP_NUM_THREADS"] = "32"
-    os.environ["KMP_BLOCKTIME"] = "30"
-    os.environ["KMP_SETTINGS"] = "1"
-    os.environ["KMP_AFFINITY"] = "granularity=fine,verbose,compact,1,0"
+    os.environ["OMP_NUM_THREADS"] = "8"
+    # os.environ["KMP_BLOCKTIME"] = "30"
+    # os.environ["KMP_SETTINGS"] = "1"
+    # os.environ["KMP_AFFINITY"] = "granularity=fine,verbose,compact,1,0"
 
     # Parse command line arguments
     parser = argparse.ArgumentParser(
