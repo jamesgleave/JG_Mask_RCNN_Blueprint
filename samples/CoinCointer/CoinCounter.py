392adb56c0a312e224ce2a44bc8a112a9a6e7c35
jamesgleave <j.gleave.james@gmail.com>
2019-07-19 13:36:09 -0700

    testing

--- samples/CoinCointer/CoinCounter.py
+++ samples/CoinCointer/CoinCounter.py
@@ -767,7 +767,7 @@ if __name__ == '__main__':
 
     print("The available devices are", get_available_devices())
 
-    config = tf.ConfigProto(device_count={'XLA_CPU:0', 'CPU:0'})
+    config = tf.ConfigProto(device_count={'CPU': 0})
     sess = tf.Session(config=config)
     tf.keras.backend.set_session(session=sess)
 
