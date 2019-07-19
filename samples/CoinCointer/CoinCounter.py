f604cb94640593770ffef172be18b5f69981ad6a
jamesgleave <j.gleave.james@gmail.com>
2019-07-19 11:58:33 -0700

    testing

--- samples/CoinCointer/CoinCounter.py
+++ samples/CoinCointer/CoinCounter.py
@@ -47,23 +47,6 @@ print("default log path:", DEFAULT_LOGS_DIR)
 #  Configurations
 ############################################################
 
-from keras import backend as K
-import tensorflow as tf
-
-NUM_PARALLEL_EXEC_UNITS = 32
-config = tf.ConfigProto(intra_op_parallelism_threads=NUM_PARALLEL_EXEC_UNITS, inter_op_parallelism_threads=2,
-                        allow_soft_placement=True, device_count={'CPU': NUM_PARALLEL_EXEC_UNITS})
-
-config.graph_options.optimizer_options.global_jit_level = tf.OptimizerOptions.ON_1
-
-session = tf.Session(config=config)
-K.set_session(session)
-
-os.environ["OMP_NUM_THREADS"] = "32"
-os.environ["KMP_BLOCKTIME"] = "30"
-os.environ["KMP_SETTINGS"] = "1"
-os.environ["KMP_AFFINITY"] = "granularity=fine,verbose,compact,1,0"
-
 # *********************************************************************************************************** #
 #                                 HOW TO TRAIN THE MODEL WITH COCO WEIGHTS                                    #
 # python3 samples/CoinCointer/CoinCounter.py train --dataset=datasets/coin/ --weights=coco --logs=logs/CoinCounterLogs
@@ -778,6 +761,23 @@ def inference(path, model_inf):
 
 if __name__ == '__main__':
     import argparse
+    from keras import backend as K
+    import tensorflow as tf
+
+    NUM_PARALLEL_EXEC_UNITS = 32
+    config = tf.ConfigProto(intra_op_parallelism_threads=NUM_PARALLEL_EXEC_UNITS, inter_op_parallelism_threads=2,
+                            allow_soft_placement=True, device_count={'CPU': NUM_PARALLEL_EXEC_UNITS})
+
+    config.graph_options.optimizer_options.global_jit_level = tf.OptimizerOptions.ON_1
+
+    session = tf.Session(config=config)
+    K.set_session(session)
+
+    os.environ["OMP_NUM_THREADS"] = "32"
+    os.environ["KMP_BLOCKTIME"] = "30"
+    os.environ["KMP_SETTINGS"] = "1"
+    os.environ["KMP_AFFINITY"] = "granularity=fine,verbose,compact,1,0"
+
     # Parse command line arguments
     parser = argparse.ArgumentParser(
         description='Train Mask R-CNN to detect coins.')
