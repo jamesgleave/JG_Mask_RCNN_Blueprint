d1a98bc14b238f5e4eda6b5ae25ab7c647ad59d3
jamesgleave <j.gleave.james@gmail.com>
2019-07-26 14:32:33 -0700

    testing

--- samples/CoinCointer/CoinCounter.py
+++ samples/CoinCointer/CoinCounter.py
@@ -73,7 +73,7 @@ class CoinConfig(Config):
 
     # We use a GPU with 12GB memory, which can fit two images.
     # Adjust down if you use a smaller GPU.
-    IMAGES_PER_GPU = 2
+    IMAGES_PER_GPU = 4
 
     GPU_COUNT = 2
 
